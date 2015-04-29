ea2873594f0e0efd9842207ee39aaee39c71413b
TRINH DUC DUY <duytd.hanu@gmail.com>
2015-04-29 09:59:32 +0700

    Add stop words

--- src/main/scala/com/portia/models/Document.scala
+++ src/main/scala/com/portia/models/Document.scala
@@ -3,6 +3,7 @@ package models
 import com.mongodb.casbah.Imports.ObjectId
 import com.novus.salat.dao.SalatDAO
 import com.novus.salat.global._
+import de.l3s.boilerpipe.extractors.ArticleExtractor
 
 /**
  * Web node html content
@@ -15,3 +16,10 @@ case class Document(_id: ObjectId = new ObjectId, urlId: ObjectId, content:Strin
 }
 object DocumentDAO extends SalatDAO[Document, ObjectId](
   collection = DB.mongoDB("documents"))
+
+object Document {
+  def extractArticle(html:String): String = {
+    val extractor = new ArticleExtractor
+    extractor.getText(html)
+  }
+}
--- src/main/scala/com/portia/models/Tokenizer.scala
+++ src/main/scala/com/portia/models/Tokenizer.scala
@@ -7,38 +7,64 @@ import com.mongodb.casbah.commons.MongoDBObject
 import org.apache.lucene.analysis._
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
+import org.apache.lucene.analysis.util.CharArraySet
+import org.apache.lucene.util.Version
 import vn.hus.nlp.tokenizer.VietTokenizer
 import java.util.ArrayList
 import org.jsoup.Jsoup
+import scala.io.Source
+
 /**
  * Tokenizer is a component which can extract tokens from a HTML web document
  * @author duytd
  */
-class Tokenizer {
+class Tokenizer(lang:String = "en") {
+  var STOP_WORDS = new CharArraySet(Version.LATEST, 0, true)
+  val viStopWordsFile = "/stop_words/vietnamese_stopwords.txt"
+  val enStopWordsFile = "/stop_words/english_stopwords.txt"
+  val filePath = if (lang == "vi") viStopWordsFile else enStopWordsFile
+
+  // load stop words
+  loadStopWords()
 
   def tokenizeDocs(documents: Array[Document]):Unit = {
 
     documents.foreach(doc => {
       var tokens = new ArrayList[String]
       tokens = tokenize(Jsoup.parse(doc.content).text())
-      saveTokensToDB(doc, tokens)
+      //saveTokensToDB(doc, tokens)
+      println(tokens.toString)
     })
   }
 
+  private def loadStopWords(): Unit ={
+    for (word <- Source.fromURL(getClass.getResource(this.filePath), "UTF-8").getLines()) {
+      if (this.lang == "vi") {
+        STOP_WORDS.add(word)
+      }
+      else {
+        STOP_WORDS.add(word)
+      }
+    }
+  }
+
   /* Method to tokenize English document using Standard Analyzer (Lucene) */
-  private def tokenize(text:String): ArrayList[String] = {
-    val analyzer = new StandardAnalyzer()
+  def tokenize(text:String): ArrayList[String] = {
+    val analyzer = new StandardAnalyzer(Version.LATEST, STOP_WORDS)
     getTokens(analyzer, text)
   }
 
   /* Method to tokenize Vietnamese document using vnTokenizer */
-  private def tokenizeVi(text: String): util.ArrayList[String] = {
+  def tokenizeVi(text: String): util.ArrayList[String] = {
     val tokenizer = new VietTokenizer
     var tokens = new ArrayList[String]
     tokenizer.tokenize(text)(0).split(" ").toList.foreach(token => {
-      //remove underscore from compound words
-      val normalizedToken = token.replace("_", " ")
-      tokens.add(normalizedToken)
+
+      if (!STOP_WORDS.contains(token)) {
+        //remove underscore from compound words
+        val normalizedToken = token.replace("_", " ")
+        tokens.add(normalizedToken)
+      }
     })
     tokens
   }
