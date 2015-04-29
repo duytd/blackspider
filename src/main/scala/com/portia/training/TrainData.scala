9971ddf7f43c589ce7f76a78ea52560de91c095b
HA Quang Minh <minhprg@gmail.com>
2015-04-29 09:58:31 +0700

    Add categories

---
+++ src/main/scala/com/portia/training/TrainData.scala
@@ -0,0 +1,13 @@
+package com.portia.training
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * @author qmha
+ */
+class TrainData {
+  var mapper:ArrayBuffer[Tuple2[String, String]] = new ArrayBuffer[Tuple2[String, String]]()
+
+  def getMapper(): Unit = {
+  }
+}
