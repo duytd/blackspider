59143112e0867ee3cb3e44779784d29d38ebd2e7
HA Quang Minh <minhprg@gmail.com>
2015-04-29 13:37:37 +0700

    Update

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ scalaVersion := "2.11.6"
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
 resolvers += "SBT Repository" at "http://repo.scala-sbt.org/scalasbt/repo/"
+resolvers += "MongoDB Cashbah" at "https://oss.sonatype.org/content/repositories/releases/"
 
 libraryDependencies += "org.mongodb" %% "casbah" % "2.8.0"
 libraryDependencies += "com.novus" %% "salat" % "1.9.9"
--- src/main/scala/com/portia/algorithms/PageRank.scala
+++ src/main/scala/com/portia/algorithms/PageRank.scala
@@ -1,8 +1,39 @@
 package com.portia.algorithms
 
+import models.Url
+
+import scala.collection.mutable.ArrayBuffer
+import com.mongodb.casbah.Imports._
+
 /**
  * @author qmha
  */
 class PageRank {
+  var iterations:Int = 10000
+  var urls: ArrayBuffer[Url] = getUrls
 
+  def run: Unit = {
+    for (i <- 0 until iterations) {
+      println("Iteration " + i)
+
+      // For all URL
+      urls.foreach(url => {
+        val pr = 0.15
+        // Loop through all pages that link to this one
+        val linkers = getEdgesByUrl(url._id)
+        linkers.foreach(linker => {
+          // Get the pagerank of the linker
+          
+        })
+      })
+    }
+  }
+
+  def getUrls:ArrayBuffer[Url] = {
+    new ArrayBuffer[Url]()
+  }
+
+  def getEdgesByUrl(url_id: ObjectId):ArrayBuffer[Url] = {
+    0
+  }
 }
